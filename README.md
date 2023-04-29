93cba0b1773dd98988ac6b915fd2cba5d2036a8d
Daksh <72447688+mrcodefrost@users.noreply.github.com>
2023-04-29 17:47:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hacksolstice_220Volts
+ Repository for Hack Solstice 2023
---
+++ android/app/src/main/java/com/mrcodefrost/hacksolstice_220volts/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mrcodefrost.hacksolstice_220volts;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
