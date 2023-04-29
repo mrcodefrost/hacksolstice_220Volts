6ecd2f5560b7cab35620e3998bd6e1a66236b9ed
Daksh <72447688+mrcodefrost@users.noreply.github.com>
2023-04-29 17:54:22 +0530

    initial

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class KustomColors {
+  static const Color mainFontColor = Color(0XFF9C9583);
+  static const Color firstSuggestionBoxColor = Color(0XFFE2D4B7);
+  static const Color secondSuggestionBoxColor = Color(0XFFCADBC8);
+  static const Color thirdSuggestionBoxColor = Color(0XFFB0BBBF);
+  static const Color assistantCircleColor = Color(0XFFE2D4B7);
+  static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Colors.white;
+}
+
+const openAIAPIKey = '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';
